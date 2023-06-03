21168ecbd90fa82d5f59b9ddba49a77adab36716
Elvin1999 <elvincamalzade00@gmail.com>
2023-06-03 12:13:28 +0400

    Add project files.

---
+++ ECommerce.Business/Concrete/ProductService.cs
@@ -0,0 +1,26 @@
+﻿using ECommerce.Business.Abstract;
+using ECommerce.DataAccess.Abstract;
+using ECommerce.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Business.Concrete
+{
+    public class ProductService : IProductService
+    {
+        private readonly IProductDal _productDal;
+
+        public ProductService(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productDal.GetList();
+        }
+    }
+}
